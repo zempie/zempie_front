cedc707aa56645b2dcdb22996296d03308045efc
hyeonjeong <hj.lee@fromthered.com>
2022-10-28 18:11:43 +0900

    main 수정

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
   <MyTimeline v-if="useCookie(config.COOKIE_NAME).value" />
-  <MainGuest v-else />
+  <template v-else>
+    <NuxtLoadingIndicator v-if="useUser().user.value.isLoading" />
+    <MainGuest v-else />
+  </template>
 </template>
 
 <script lang="ts" setup>
 const config = useRuntimeConfig()
+
+const showTimeline = computed(() => {
+  return useCookie(config.COOKIE_NAME).value && useUser().user.value.isLogin
+})
 
 definePageMeta({
   title: 'main',
