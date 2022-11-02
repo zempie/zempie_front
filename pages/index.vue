4bd044d7c50a181eb0bee2b1a670c96e018cbfe1
hyeonjeong <hj.lee@fromthered.com>
2022-11-02 10:35:52 +0900

    index 수정

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,9 @@
 <template>
   <div>
-    <template v-if="useUser().user.value.isLoading">
-      <NuxtLoadingIndicator />
-    </template>
-    <template v-else>
-      <MyTimeline
-        v-if="
-          useCookie(config.COOKIE_NAME).value && useUser().user.value.isLogin
-        "
-      />
-      <MainGuest v-else />
-    </template>
+    <MyTimeline
+      v-if="useCookie(config.COOKIE_NAME).value && useUser().user.value.isLogin"
+    />
+    <MainGuest v-else />
   </div>
 </template>
 
