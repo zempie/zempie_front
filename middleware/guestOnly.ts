e3c19228e6e96f66f5a1e83eeb2137867b5be5cd
hyeonjeong <hj.lee@fromthered.com>
2022-12-13 14:52:15 +0900

    ..

--- middleware/guestOnly.ts
+++ middleware/guestOnly.ts
@@ -4,7 +4,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const cookie = useCookie(config.COOKIE_NAME).value;
   const info = useUser().user.value.info
 
-  if (!info) {
+  if (info) {
     return '/';
   }
 
