03db2f811b53ae1b5891351d3e6904413d21fa2c
hyeonjeong <hj.lee@fromthered.com>
2023-01-11 10:22:53 +0900

    ..

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div v-if="useUser().user.value.isLoading" class="content"></div>
-  <div v-else :key="useUser().user.value.isLogin">
+  <div v-else>
     <MyTimeline
-      v-if="userCookie && useUser().user.value.isLogin"
+      v-if="userCookie "
     />
     <MainGuest v-else/>
   </div>
@@ -17,15 +17,6 @@ const userCookie = useCookie(config.COOKIE_NAME)
 definePageMeta({
   title: 'main',
   name: 'main',
-})
-
-onBeforeMount(async()=>{
-  // if(userCookie.value && !useUser().user.value.isLogin){
-  //   colorLog("index page", 'yellow')
-
-  //   await useUser().setUserInfo()
-
-  //   }
 })
 </script>
 
