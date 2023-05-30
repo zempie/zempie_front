1beed380bbf047274ba12ba95c2e48135d8192d1
hyeonjeong <hj.lee@fromthered.com>
2023-05-30 14:42:38 +0900

    script 변경

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,14 +33,14 @@ export default defineNuxtConfig({
           src: process.env.KAKAO_SDK
         },
         {
-          hid: 'gtmHead',
-          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-         })(window,document,'script','dataLayer',${process.env.TAG_MANAGER_ID});`
+          children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer',${process.env.TAG_MANAGER_ID});`
         }
       ],
+
 
     },
   },
