2f8264868e2e7bd45437f9dc81bad286c453a6fe
hyeonjeong <hj.lee@fromthered.com>
2023-05-30 16:00:45 +0900

    gtm 변경

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer',${process.env.TAG_MANAGER_ID});`
+        })(window,document,'script','dataLayer','${process.env.TAG_MANAGER_ID}');`
         }
       ],
 
