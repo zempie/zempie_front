9c204075f979fb566b8e4cb64181e70ec95b1718
hyeonjeong <hj.lee@fromthered.com>
2023-06-26 19:15:07 +0900

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,18 @@ export default defineNuxtConfig({
         path: '/community/:id/:channel_name',
         file: resolve(__dirname, './pages/community/[id]/index.vue')
       })
-    }
+    },
+    'build:manifest': (manifest) => {
+      // find the app entry, css list
+      const css = manifest['node_modules/nuxt/dist/app/entry.js']?.css
+      if (css) {
+        // start from the end of the array and go to the beginning
+        for (let i = css.length - 1; i >= 0; i--) {
+          // if it starts with 'entry', remove it from the list
+          if (css[i].startsWith('entry')) css.splice(i, 1)
+        }
+      }
+    },
   },
   generate: {
     fallback: '404.html'
@@ -59,6 +70,7 @@ export default defineNuxtConfig({
   router: {
     middleware: ['auth']
   },
+
   css: [
     '@/assets/css/jquery-ui.css',
     '@/assets/css/layout.css',
@@ -75,7 +87,14 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     ['nuxt-compress', { gzip: { threshold: 8192 } }]
   ],
+  nitro: {
+    compressPublicAssets: true,
+    minify: true
+  },
 
+  build: {
+    extractCSS: true,
+  },
 
   publicRuntimeConfig: {
     ENV: process.env.ENV,
