078c904b6def3274aef5240fe909d5289ad6fd72
hyeonjeong <hj.lee@fromthered.com>
2023-08-24 15:49:32 +0900

    ..

--- app.vue
+++ app.vue
@@ -54,7 +54,7 @@ onBeforeMount(async () => {
       useUser().setLoadDone()
     }
     flutterBridge().webLog(`fuser: ${fUser}`)
-    console.log(fUser)
+    console.log(fUser && fUser.accessToken)
     useZemtown().setUrl(`${config.ZEMTOWN_URL}?token=${fUser?.accessToken}`)
 
     if (isFlutter.value && fUser && !userInfo.value) {
