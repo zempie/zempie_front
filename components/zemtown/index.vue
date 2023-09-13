8d2382a3bc5f990872eb11acb17a49164d43c490
hyeonjeong <hj.lee@fromthered.com>
2023-09-13 18:51:04 +0900

    zemtown

--- components/zemtown/index.vue
+++ components/zemtown/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <iframe ref="zemtownRef" :src="zemtownUrl" style="width: 100%;" /> -->
+  <iframe ref="zemtownRef" :src="zemtownUrl" style="width: 100%;" />
 </template>
 <script setup lang="ts">
 const config = useRuntimeConfig()
